63fc4ee1d69bc80c7c05703d5bee7475231e4e49
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-01-15 16:10:09 +0000

    Adds some actions to handle approving and deactivating accounts.

---
+++ src/Actions/ApproveAccount.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Creode\LaravelNovaAccountApproval\Actions;
+
+class ApproveAccount extends AccountApproval
+{
+    /**
+     * Activation Status to toggle.
+     *
+     * @var boolean
+     */
+    protected $activationStatus = true;
+}
---
+++ src/Actions/DeactivateAccount.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Creode\LaravelNovaAccountApproval\Actions;
+
+class DeactivateAccount extends AccountApproval
+{
+    /**
+     * Activation Status to toggle.
+     *
+     * @var boolean
+     */
+    protected $activationStatus = false;
+}
