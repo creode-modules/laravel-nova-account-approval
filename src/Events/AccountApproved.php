a3a96092b968fef5bb3e4f8d53075dc372f728f9
Liam Spedding <liam.spedding@creode.co.uk>
2024-06-27 15:00:39 +0100

    Create Approved and Denied Events

---
+++ src/Events/AccountApproved.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Creode\LaravelNovaAccountApproval\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class AccountApproved
+{
+    use SerializesModels, Dispatchable;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public array $data)
+    {
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     */
+    public function broadcastOn(): array
+    {
+        return [];
+    }
+}
